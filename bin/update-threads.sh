fab314456a6e460d37bd51d8cadc679e9c48cd05
MikeD <zxmiked@gmail.com>
2017-11-18 05:45:03 -0500

    update to use var/ as the output directory for workfiles.

--- bin/update-threads.sh
+++ bin/update-threads.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-THREAD_FILE="updated-threads.csv"
+THREAD_LIST="var/threads-update.csv"
 COUNT=0
 
 while read LINE; do
@@ -11,8 +11,8 @@ while read LINE; do
 
     THREAD_URL=${FIELDS[1]}
     echo "Retrieving: $THREAD_URL"
-    node bin/scrape-thread.js $THREAD_URL
-done < $THREAD_FILE
+    node bin/scrape-thread.js $THREAD_URL $THREAD_FILE
+done < $THREAD_LIST
 
 echo "$COUNT threads updated."
 
